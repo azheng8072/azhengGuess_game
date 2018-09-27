2eac812e93c4cfdd4c3ac3ac7b4e727ca4d48388
azheng8072 <43213098+azheng8072@users.noreply.github.com>
2018-09-27 11:05:23 -0400

    commented original on the bottom

--- src/azheng/azhengLib.java
+++ src/azheng/azhengLib.java
@@ -71,3 +71,51 @@ public class azhengLib
 
 
 }
+
+// package azheng;
+// import java.util.Scanner;
+// import java.lang.Math;
+// public class azhengLib
+// {
+//     public static void guessGameObj1() {
+//         int number = (int) (10.0 * Math.random());
+//         System.out.println("Type in your name");
+//         Scanner name = new Scanner(System.in);
+//         String username = name.nextLine();
+//         int guessedNum = 0;
+//         while (guessedNum != number) {
+//             System.out.println("Guess a number");
+//             Scanner searchNum = new Scanner(System.in);
+//             guessedNum = searchNum.nextInt();
+//         }
+//         if (guessedNum == number) {
+//             System.out.println(username + ", you guessed the number!");
+//         }
+//     }
+
+
+
+//         public static void guessGameObj2() {
+//             int number = (int) (10.0 * Math.random());
+//             System.out.println("Type in your name");
+//             Scanner name = new Scanner(System.in);
+//             String username = name.nextLine();
+//             int guessedNum = 0;
+//             while (guessedNum != number) {
+
+//                 System.out.println("Guess a number");
+//                 Scanner searchNum = new Scanner(System.in);
+//                 guessedNum = searchNum.nextInt();
+//                 if (guessedNum < number) {
+//                     System.out.println("The number is greater than what you guessed");
+//                 } else {
+//                     if (guessedNum > number) {
+//                         System.out.println("The number is less than what you guessed");
+//                     }
+//                 }
+//             }
+//             if (guessedNum == number) {
+//                 System.out.println(username + ", you guessed the number!");
+//             }
+//         }
+// }
